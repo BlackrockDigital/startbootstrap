d9a8e03f6efcca040ff47381d13edbe20186abdc
David Miller <hello@davidmiller.io>
2016-02-04 14:20:50 -0500

    starting to add search functionality

---
+++ assets/js/search.js
@@ -0,0 +1,58 @@
+jQuery(function() {
+  // Initalize lunr with the fields it will be searching on. I've given title
+  // a boost of 10 to indicate matches on this field are more important.
+  window.idx = lunr(function () {
+    this.field('id');
+    this.field('title', { boost: 10 });
+    this.field('author');
+    this.field('categories');
+    this.field('source');
+    this.field('description');
+  });
+
+  // Download the data from the JSON file we generated
+  window.data = $.getJSON('/search_data.json');
+
+  // Wait for the data to load and add it to lunr
+  window.data.then(function(loaded_data){
+    $.each(loaded_data, function(index, value){
+      window.idx.add(
+        $.extend({ "id": index }, value)
+      );
+    });
+  });
+
+  // Event when the form is submitted
+  $("#site_search").submit(function(){
+      event.preventDefault();
+      var query = $("#search_box").val(); // Get the value for the text field
+      var results = window.idx.search(query); // Get lunr to perform a search
+      display_search_results(results); // Hand the results off to be displayed
+  });
+
+  function display_search_results(results) {
+    var $search_results = $("#search_results");
+
+    // Wait for data to load
+    window.data.then(function(loaded_data) {
+
+      // Are there any results?
+      if (results.length) {
+        $search_results.empty(); // Clear any old results
+
+        // Iterate over the results
+        results.forEach(function(result) {
+          var item = loaded_data[result.ref];
+
+          // Build a snippet of HTML for this result
+          var appendString = '<li><a href="' + item.source + '">' + item.title + '</a> - ' + item.description + '</li>';
+
+          // Add it to the results
+          $search_results.append(appendString);
+        });
+      } else {
+        $search_results.html('<li>No results found</li>');
+      }
+    });
+  }
+});
