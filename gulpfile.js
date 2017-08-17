a03e23e1672be2a151bf7b90bbf75f2861a22648
David Miller <hello@davidmiller.io>
2017-08-17 17:21:42 -0400

    Update dependencies and padding fix on resources page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@
 var gulp = require('gulp');
 
 // Copy Bootstrap core files from node_modules to vendor directory
-gulp.task('default', function() {
-    return gulp.src(['node_modules/bootstrap/scss/**/*'])
-        .pipe(gulp.dest('bootstrap'))
+gulp.task('copy', function() {
+
+  gulp.src(['node_modules/bootstrap/dist/js/*.js'])
+    .pipe(gulp.dest('assets/vendor/bootstrap/js'))
+
+  gulp.src(['node_modules/bootstrap/scss/**/*'])
+    .pipe(gulp.dest('assets/vendor/bootstrap/scss'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('assets/vendor/jquery'))
+
+  gulp.src(['node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest('assets/vendor/popper'))
+
 })
